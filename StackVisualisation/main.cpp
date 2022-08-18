dfb260da5aaa798b8b64c1c309db403e42ea0022
forgen00 <111133116+forgen00@users.noreply.github.com>
2022-08-18 14:53:02 +0500

    Update main.cpp

--- StackVisualisation/main.cpp
+++ StackVisualisation/main.cpp
@@ -20,6 +20,7 @@ int main() {
         button_left += button_width;
     }
 
+    bool fl = false;
     Stack s = empty_stack();
     
     while (1)
@@ -28,8 +29,141 @@ int main() {
         clearviewport(); // закраска текущей страницы цветом фона
         ++t;
         // рисование изображения
+        rectangle(300, 125, WIDTH - 300, 875);
         drawButtons(WIDTH, HEIGHT);
+        if (is_full(s)) {
+            setbkcolor(BLACK);
+            outtextxy(50, HEIGHT / 2, "Stack");
+            outtextxy(50, HEIGHT / 2 + 30, "is");
+            outtextxy(50, HEIGHT / 2 + 60, "full!");
+        }
+        StackVisual(s, WIDTH, HEIGHT);
         
+        
+
+        if (mousebuttons() == 1) {
+            int button = select_control();
+
+            if (button == BUTTON_1) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_1);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_2) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_2);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_3) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_3);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_4) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_4);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_5) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_5);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_6) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_6);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_7) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_7);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_8) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_8);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_9) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_9);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_10) {
+                if (is_full(s)) {
+                    std::cout << "Stack is full\n";
+                }
+
+                else {
+                    push(s, BUTTON_10);
+                    fl = false;
+                }
+            }
+            if (button == BUTTON_11) {
+                if (is_empty(s)) {
+                    std::cout << "Stack is empty\n";
+                    fl = true;
+                }
+
+                else {
+                    pop(s);
+                    fl = false;
+                }
+            }
+        }
+
+        if (fl) {
+            setbkcolor(BLACK);
+            outtextxy(1500, HEIGHT / 2, "Stack");
+            outtextxy(1500, HEIGHT / 2 + 30, "is");
+            outtextxy(1500, HEIGHT / 2 + 60, "empty!");
+        }
+
         
         swapbuffers(); // делаем активную страницу видимой
         delay(30); // задержка на 1/30 секунды
