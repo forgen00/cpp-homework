670309e8a13f6c1b57401dd9a10d228c1665e22d
forgen00 <111133116+forgen00@users.noreply.github.com>
2022-09-09 21:21:41 +0500

    Add files via upload

---
+++ Linked list improved/LinkedList.h
@@ -0,0 +1,23 @@
+#ifndef _LINKED_LIST_H
+#define _LINKED_LIST_H
+
+class List {
+private:
+	struct Node {
+		int value;
+		Node* next;
+		Node* prev;
+	}*head;
+
+public:
+	List();								//Конструктор
+	List(const List& other);			//Конструктор копирования
+	~List();							//Деструктор (удаляет список)
+	Node* find_node(int value);
+	void push_front(int value);
+	void push_back(int value);
+	void remove(Node* target);
+	void print();
+};
+
+#endif
